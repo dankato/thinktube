7e2b5f846aac06b7e31be75497a89c90c400bf01
joncancode <jc.haines19@gmail.com>
2017-06-21 17:01:13 -0400

    finished basic app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // Optional
 // Make the images clickable, leading to the YouTube video, on YouTube [done]
 // Make the images clickable, playing them in a lightbox
-// Show a link for more from the channel that each video came from
+// Show a link for more from the channel that each video came from [done]
 // Show buttons to get more results (using the previous and next page links from the JSON) (max results)
 
 // app state
@@ -17,43 +17,39 @@ const appState = {
 
 // https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyBlKXL_oW-vqf4DFrs6dov1ddwSt6fyscI&q=javascript
 
-// state mod functions
 
+// controller (mvc)
 // getting data from api
-function getData(searchTerm, callback) {
-    const url = "https://www.googleapis.com/youtube/v3/search";
+function getData(endpointURL, searchTerm, callback) {
+    //const url = "https://www.googleapis.com/youtube/v3/search";
     const query = {
         part: 'snippet',
         key: 'AIzaSyBlKXL_oW-vqf4DFrs6dov1ddwSt6fyscI',
         q: searchTerm,
     }
-    $.getJSON(url, query, callback);
+    $.getJSON(endpointURL, query, callback);
 }
 
-
-function storeData (data) {
-    console.log('i am in storeData');
-    console.log(data);
-    appState.results = data.items; // not sure how to write this out 
-    // console.log("appstate", appState.results);
-    // console.log("data items", data.items);
+// state mod functions
+function storeData(data) {
+    appState.results = data.items;
     render(appState);
 }
 
-
 // render
 const render = (appState) => {
-    console.log('im in render');
     var searchResults = "";
-    console.log('search results:' + searchResults);
     appState.results.forEach(function (items) {
-        console.log($(items))
-        searchResults += `<h3>${items.snippet.title}</h3>
+        searchResults += (`
         <a class="link" href="https://www.youtube.com/watch?v=${items.id.videoId}">
-     <img class="thumbnail" src=${items.snippet.thumbnails.medium.url}></a>
+            <h3>${items.snippet.title}</h3>
+            <img class="thumbnail" src=${items.snippet.thumbnails.medium.url}>
+        </a>
         <br>
-        <a class="link" href="https://www.youtube.com/channel/${items.snippet.channelId}">More from the ${items.snippet.channelTitle} Channel</a>
-        `});
+        <a class="link" href="https://www.youtube.com/channel/${items.snippet.channelId}">
+            More from the ${items.snippet.channelTitle} Channel
+        </a>
+        `)});
     $('.js-search-results').html(searchResults)
 };
 
@@ -61,15 +57,12 @@ const render = (appState) => {
 // event handler
 $('.js-search-form').on('submit', (event) => {
     event.preventDefault();
+    const endpointURL = $(event.target).attr('action')
     const getInput = $(event.target).find('input').val();
-    console.log("user submitted: " + getInput);
-    getData(getInput, storeData)   
+    getData(endpointURL, getInput, storeData)
 })
 
-// execute
-
-/*
-      //${items.snippet.thumbnails.medium.url}
-        `<a href="https://www.youtube.com/watch?v=${items.id.videoId}">link</a>
+/* 
+on click -> grab input value -> runs getDate with input value & holds on to storeData -> once inside getData, takes input value and create URL, calls URL -> runs storeData -> take results and add to appState results [] -> render appState -> create empty container -> grabs results and runs function for each result passed it, function is create template and inputs the values for each result in its own template -> prints the results container to the html
 
 */
